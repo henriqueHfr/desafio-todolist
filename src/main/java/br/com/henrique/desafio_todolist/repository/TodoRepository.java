6cbe0c8d89dff9e46804adb1408a21dd6a8ad112
Henrique Rodrigues <henrique.hfr2006@gmail.com>
2024-09-14 13:50:34 -0300

    Criação do service, repository e models

---
+++ src/main/java/br/com/henrique/desafio_todolist/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package br.com.henrique.desafio_todolist.repository;
+
+import br.com.henrique.desafio_todolist.models.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
---
+++ src/main/java/br/com/henrique/desafio_todolist/service/TodoService.java
@@ -0,0 +1,34 @@
+package br.com.henrique.desafio_todolist.service;
+
+import br.com.henrique.desafio_todolist.models.Todo;
+import br.com.henrique.desafio_todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> list() {
+        Sort sort = Sort.by(Sort.Order.desc("prioridade"), Sort.Order.asc("nome"));
+        return todoRepository.findAll(sort);
+    }
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
