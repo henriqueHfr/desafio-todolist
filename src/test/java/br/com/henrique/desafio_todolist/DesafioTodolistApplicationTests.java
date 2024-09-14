a0bcd89600c3332690c613f6d053621f302b2dba
Henrique Rodrigues <henrique.hfr2006@gmail.com>
2024-09-14 13:35:06 -0300

    Inicando o projet e estrutura de pasta

---
+++ src/test/java/br/com/henrique/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henrique.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
